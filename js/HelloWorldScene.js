654e0647548e3fa8841efbead432a167443f72fb
Thomas Lacroix <t.f.m.lacroix@gmail.com>
2019-12-11 11:24:25 +0100

    added first iteration

--- js/HelloWorldScene.js
+++ js/HelloWorldScene.js
@@ -7,7 +7,14 @@ import {StyleSheet} from 'react-native';
 import {
   ViroScene,
   ViroText,
-  Viro360Image,
+  ViroSkyBox,
+  ViroBox,
+  ViroMaterials,
+  ViroAmbientLight,
+  ViroAnimations,
+  ViroCamera,
+  Viro3DObject,
+  ViroNode
 } from 'react-viro';
 
 export default class HelloWorldScene extends Component {
@@ -19,24 +26,63 @@ export default class HelloWorldScene extends Component {
   }
 
   render() {
+    var positionTreeRight = [];
+    var positionTreeLeft = [];
+    for (let index = 0; index < 100; index++) {
+      positionTreeRight.push([10 +Math.random() * 100, -4, Math.random() * 200])
+      positionTreeLeft.push([ -10 - Math.random() * 100, -4, Math.random() * 200])
+    }
+
     return (
       <ViroScene>
-        <Viro360Image source={require('./res/guadalupe_360.jpg')} />
-        <ViroText text="Hello World!" width={2} height={2} position={[0, 0, -2]} style={styles.helloWorldTextStyle} />
+        <ViroCamera position={[0,0,0]} active={true} >
+        </ViroCamera>
+        <ViroNode position={[0, 7, -200]} animation={{name:'loopRotateSlow', run:true, loop:true}}>
+          {positionTreeRight.map(function(positionTreeRight, index){
+            return <Viro3DObject source={require('./res/lowpolytree.obj')} key={ index } resources={[require('./res/lowpolytree.mtl')]} position={positionTreeRight} scale={[5, 5, 5]} type="OBJ"/>;
+          })}
+
+          {positionTreeLeft.map(function(positionTreeLeft, index){
+            return <Viro3DObject source={require('./res/lowpolytree.obj')} key={ index } resources={[require('./res/lowpolytree.mtl')]} position={positionTreeLeft} scale={[5, 5, 5]} type="OBJ"/>;
+          })}
+        </ViroNode>
+        <ViroSkyBox color="#add8e6"/>
+        <ViroAmbientLight color="#FFFFFF" intensity={3000}/>
+        <ViroBox position={[0, -5, -50]} scale={[200,.5,200]} materials={["grid"]} />
+        <ViroBox position={[0, -4.5, -200]} scale={[10,.5,200]} materials={["road"]} animation={{name:'loopRotate', run:true, loop:true}}/>
+        <ViroBox position={[0, -4.5, -400]} scale={[10,.5,200]} materials={["road"]} animation={{name:'loopRotate', run:true, loop:true}}/>
       </ViroScene>
     );
+  }
+
+  _onAnimationFinished(){
+    console.log("Animation has finished!");
   }
 
 }
 
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 60,
+    fontFamily: 'Roboto',
+    fontSize: 20,
     color: '#ffffff',
     textAlignVertical: 'center',
     textAlign: 'center',  
   },
+});
+
+ViroMaterials.createMaterials({
+  grid: {
+    diffuseTexture: require('./res/grass.jpg'),
+  },
+  road: {
+    diffuseTexture: require('./res/road.jpg'),
+  },
+});
+
+ViroAnimations.registerAnimations({
+  loopRotate:{properties:{positionZ:"+=1"}, duration:300},
+  loopRotateSlow:{properties:{positionZ:"+=1"}, duration:300},
 });
 
 module.exports = HelloWorldScene;
