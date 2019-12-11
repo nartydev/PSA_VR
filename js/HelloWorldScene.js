de5b2d30e84b70871d828f3cdafaa0685a7d2dec
Kérian Pelat <narty.dev@gmail.com>
2019-12-11 11:41:21 +0100

    nul

--- js/HelloWorldScene.js
+++ js/HelloWorldScene.js
@@ -2,41 +2,68 @@
 
 import React, { Component } from 'react';
 
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import {
-  ViroScene,
-  ViroText,
-  Viro360Image,
+	ViroScene,
+	ViroMaterials,
+	ViroAmbientLight,
+	ViroBox,
+	Viro3DObject,
+	ViroSpotLight,
+	ViroOmniLight
 } from 'react-viro';
 
 export default class HelloWorldScene extends Component {
+	constructor() {
+		super();
 
-  constructor() {
-    super();
+		this.state = {}; // Set initial state here
+	}
+	render() {
+		ViroMaterials.createMaterials({
+			grass: {
+				lightingModel: 'Blinn',
+				diffuseTexture: require('./res/grass.jpg'),
+				specularTexture: require('./res/grass.jpg')
+			}
+		});
+		return (
+			<ViroScene>
+				<ViroAmbientLight color="#ffffff" />
 
-    this.state = {} // Set initial state here
-  }
-
-  render() {
-    return (
-      <ViroScene>
-        <Viro360Image source={require('./res/guadalupe_360.jpg')} />
-        <ViroText text="Hello World!" width={2} height={2} position={[0, 0, -2]} style={styles.helloWorldTextStyle} />
-      </ViroScene>
-    );
-  }
-
+				<Viro3DObject
+					source={require('./res/lowpolytree.obj')}
+					materials={[ 'grass' ]}
+					position={[ 2.0, 0, -2.0 ]}
+					type="OBJ"
+				/>
+				<Viro3DObject
+					source={require('./res/lowpolytree.obj')}
+					materials={[ 'grass' ]}
+					position={[ 3.0, 0, 0.0 ]}
+					type="OBJ"
+				/>
+				<Viro3DObject
+					source={require('./res/lowpolytree.obj')}
+					materials={[ 'grass' ]}
+					position={[ 4.0, 0, 2.0 ]}
+					type="OBJ"
+				/>
+				<ViroBox height={15} length={100} width={100} position={[ 0.0, -20.0, 0.0 ]} materials={[ 'grass' ]} />
+			</ViroScene>
+		);
+	}
 }
 
 var styles = StyleSheet.create({
-  helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 60,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',  
-  },
+	helloWorldTextStyle: {
+		fontFamily: 'Arial',
+		fontSize: 60,
+		color: '#ffffff',
+		textAlignVertical: 'center',
+		textAlign: 'center'
+	}
 });
 
 module.exports = HelloWorldScene;
